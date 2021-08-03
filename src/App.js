22a753e4f1c72993040773ecb40f6aedaafadb4e
tcw-cyborg <tcwong007@gmail.com>
2021-08-03 03:14:22 +0200

    push three

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import TopStreams from "./components/TopStreams/TopStreams";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Live from "./components/Live/Live";
 import GameStreams from "./components/GameStreams/GameStreams";
+import Resultats from "./components/Resultats/Resultats";
+import Erreur from "./components/Erreur/Erreur";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
           <Route exact path="/top-streams" component={TopStreams} />
           <Route exact path="/live/:slug" component={Live} />
           <Route exact path="/game/:slug" component={GameStreams} />
+          <Route exact path="/resultats/:slug" component={Resultats} />
+          <Route exact path="/resultats/" component={Erreur} />
         </Switch>
       </div>
     </Router>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,41 +1,105 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./IconeTwitch.svg";
 import search from "./Search.svg";
 import menuIco from "./MenuIco.svg";
+import croix from "./Croix.svg";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [menu, showMenu] = useState(false);
+  const [smallScreen, setSmallScreen] = useState(false);
+  const [searchInput, setSearch] = useState("");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 900px)");
+    // addlistener = addeventlisterner pour les medias queries en JS
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  });
+
+  const handleMediaQueryChange = (mediaQuery) => {
+    if (mediaQuery.matches) {
+      setSmallScreen(true);
+    } else {
+      setSmallScreen(false);
+    }
+  };
+
+  const toggleNavRes = () => {
+    showMenu(!menu);
+  };
+
+  const hideMenu = () => {
+    if (menu === true) {
+      showMenu(!menu);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleKeyPress = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div>
       <nav className="headerTop">
-        <ul className="listeMenu">
-          <li className="liensNav">
-            <Link className="lien" to="/">
-              <img src={logo} alt="logo twitch" className="logo" />
-            </Link>
-          </li>
-          <li className="liensNav">
-            <Link className="lien" to="/">
-              Top Games
-            </Link>
-          </li>
-          <li className="liensNav">
-            <Link className="lien" to="/top-streams">
-              Top Streams
-            </Link>
-          </li>
-          <li className="liensNav">
-            <form className="formSubmit">
-              <input type="text" className="inputRecherche" />
-              <button type="submit">
-                <img src={search} alt="icone loupe" className="logoLoupe" />
-              </button>
-            </form>
-          </li>
-        </ul>
+        {(menu || !smallScreen) && (
+          <ul className="listeMenu">
+            <li onClick={hideMenu} className="liensNav">
+              <Link className="lien" to="/">
+                <img src={logo} alt="logo twitch" className="logo" />
+              </Link>
+            </li>
+            <li onClick={hideMenu} className="liensNav">
+              <Link className="lien" to="/">
+                Top Games
+              </Link>
+            </li>
+            <li onClick={hideMenu} className="liensNav">
+              <Link className="lien" to="/top-streams">
+                Top Streams
+              </Link>
+            </li>
+            <li className="liensNav">
+              <form className="formSubmit" onSubmit={handleSubmit}>
+                <input
+                  required
+                  value={searchInput}
+                  onChange={(e) => handleKeyPress(e)}
+                  type="text"
+                  className="inputRecherche"
+                />
+
+                <Link
+                  className="lien"
+                  to={{
+                    pathname: `/resultats/${searchInput}`,
+                  }}
+                >
+                  <button type="submit">
+                    <img src={search} alt="icone loupe" className="logoLoupe" />
+                  </button>
+                </Link>
+              </form>
+            </li>
+          </ul>
+        )}
       </nav>
+
       <div className="menuResBtn">
-        <img src={menuIco} alt="icone menu responsive" className="menuIco" />
+        <img
+          onClick={toggleNavRes}
+          src={!menu ? menuIco : croix}
+          alt="icone menu responsive"
+          className="menuIco"
+        />
       </div>
     </div>
   );
